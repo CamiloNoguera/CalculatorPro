01a1d51e1664c1082162660a062b36fe566779b6
CamiloNoguera <camiloandresnoguerabuelvas@gmail.com>
2021-12-03 21:46:40 -0500

    Boton Limpiar Completo

--- src/ControllerCalculator.java
+++ src/ControllerCalculator.java
@@ -1,11 +1,11 @@
 import java.net.URL;
 import java.util.ResourceBundle;
-
 import javafx.beans.Observable;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
 
@@ -27,23 +27,27 @@ public class ControllerCalculator {
     private ChoiceBox<String> Combo;
 
     @FXML
+    private Button btn;
+    ObservableList<String> list = FXCollections.observableArrayList("1. Binario a octal","2. Binario a decimal","3. Binario a hexadecimal","4. Octal a binario","5. Octal a decimal","6. Octal a hexadecimal","7. Decimal a binario","8. Decimal a octal","9. Decimal a hexadecimal","10. Hexadecimal a binario","11. Hexadecimal a octal","12. Hexadecimal a decimal");
+
+    @FXML
     void btnCalcular(ActionEvent event) {
-        //int op1 = Integer.parseInt(Cant.getText());
-        String op1 = Cant.getText();
-        Result.setText(op1);
+        int op1 = Integer.parseInt(Cant.getText());
+        
     }
 
     @FXML
     void btnLimpiar(ActionEvent event) {
         Cant.setText("");
         Result.setText("");
-        Cant.requestFocus();
-        //Combo.
+        Cant.requestFocus(); 
+        this.Combo.getSelectionModel().clearSelection();
+        this.Combo.setValue(null);
     }
 
     @FXML
     void initialize() {
-        ObservableList<String> list = FXCollections.observableArrayList("1. Binario a octal","2. Binario a decimal","3. Binario a hexadecimal","4. Octal a binario","5. Octal a decimal","6. Octal a hexadecimal","7. Decimal a binario","8. Decimal a octal","9. Decimal a hexadecimal","10. Hexadecimal a binario","11. Hexadecimal a octal","12. Hexadecimal a decimal");
         Combo.setItems(list);
+        
     }
 }
